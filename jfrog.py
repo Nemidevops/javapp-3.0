8ebedfdcf2c04c3a49a322c46d99320eb0b02da1
nemidevops <nemivijay43@gmail.com>
2024-02-12 18:36:57 +0530

    py added

--- jfrog.py
+++ jfrog.py
@@ -20,17 +20,6 @@ def jfrogUpload() :
         print("PUT request failed with status code (response.status_code)")
         print("response content:")
         print(response.text)
-def mvnBuild() :
-    #define the Maven Command
-    maven_command = "mvn clena install -DskipTests"
-
-    #Run the maven command as a subprocess
-    try:
-        subprocess.run(maven_command, check=True, text=True, shell=True)
-        print("/nMaven build completed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: Maven Build failed with exit code (e.returncode)")
-
 def main() :
     #mvnBuild()
     jfrogUpload()
